6869684e064935d812614919a47f3824c66657d8
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-06-02 04:23:05 +0530

    Starting new image data approch

--- cvdata.py
+++ cvdata.py
@@ -8,3 +8,6 @@ image = cv2.resize(image,(400,900))
 image = image[320:700,20:380]
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+
+
